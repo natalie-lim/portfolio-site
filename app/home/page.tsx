9df1155d2fbad237f80325330842d8a33fd781a7
Natalie Lim <natalielim1906@gmail.com>
2026-05-18 22:27:09 -0400

    home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,14 @@
 export default function HomePage() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-slate-950 font-sans text-white">
-      <div className="animate-[fade-in_0.5s_ease-out_forwards]">
-        <h1 className="text-3xl font-semibold">Portfolio</h1>
-        <p className="mt-4 text-zinc-400">Welcome.</p>
+    <div className="flex flex-1 grid-cols-2 items-center justify-between m-36 bg-slate-950 font-sans text-white animate-[fade-in_0.5s_ease-out_forwards]">
+      <div className="">
+        <h1 className="text-3xl font-semibold">welcome to my brain</h1>
+        <p className="mt-4 text-zinc-400">
+          full-stack. machine learning. neuro. design.
+        </p>
+      </div>
+      <div>
+        <p>insert brain here</p>
       </div>
     </div>
   );
